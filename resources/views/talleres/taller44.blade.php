670a816735f84a2d9467ee8bbb3ffa9df3901b9c
tony98ms <62909225+tony98ms@users.noreply.github.com>
2020-11-01 01:26:54 -0500

    Validación taller 44

--- resources/views/talleres/taller44.blade.php
+++ resources/views/talleres/taller44.blade.php
@@ -13,23 +13,23 @@
 				<input type="hidden" value="activo">
 					<div class="col-10">
 					<div class="row mb-3">
-						<div class="col-4 align-self-center"><textarea class="form-control area1" name="activos[]" ></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1" name="activos[]" ></textarea></div>
 						<div class="col-4 border border-success rounded-circle p-3">
 							<p class="text-center">Activo <br> Corriente</p>
 						</div>
-						<div class="col-4 align-self-center"><textarea class="form-control area1"  name="activos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1"  name="activos[]"></textarea></div>
 					</div>
 
 					<div class="row mb-3">
-						<div class="col-4 align-self-center"><textarea class="form-control area"  name="activos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area"  name="activos[]"></textarea></div>
 						<div class="col-4 border border-info rounded-circle p-3">
 							<p class="text-center">Activo <br>no <br> Corriente</p>
 						</div>
-						<div class="col-4 align-self-center"><textarea class="form-control area" name="activos[]" ></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area" name="activos[]" ></textarea></div>
 					</div>
 
 					<div class="row justify-content-center">
-						<div class="col-4 align-self-center"><textarea class="form-control area"  name="activos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area"  name="activos[]"></textarea></div>
 					</div>
 				</div>
 
@@ -38,37 +38,37 @@
 
 					<div class="col-10">
 					<div class="row mb-3">
-						<div class="col-4 align-self-center"><textarea class="form-control area1" name="pasivos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1" name="pasivos[]"></textarea></div>
 						<div class="col-4 border border-success rounded-circle p-3">
 							<p class="text-center">Pasivo <br> Corriente</p>
 						</div>
-						<div class="col-4 align-self-center"><textarea class="form-control area1" name="pasivos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1" name="pasivos[]"></textarea></div>
 					</div>
 
 					<div class="row mb-3">
-						<div class="col-4 align-self-center"><textarea class="form-control area" name="pasivos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area" name="pasivos[]"></textarea></div>
 						<div class="col-4 border border-info rounded-circle p-3">
 							<p class="text-center">Pasivo <br>no <br> Corriente</p>
 						</div>
-						<div class="col-4 align-self-center"><textarea class="form-control area" name="pasivos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area" name="pasivos[]"></textarea></div>
 					</div>
 
 					<div class="row justify-content-center">
-						<div class="col-4 align-self-center"><textarea class="form-control area" name="pasivos[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area" name="pasivos[]"></textarea></div>
 					</div>
 				</div>
 				@elseif ($datos->cuenta == 'patrimonio')
 				<input type="hidden" value="patrimonio">
 					<div class="col-10">
 					<div class="row mb-3">
-						<div class="col-4 align-self-center"><textarea class="form-control area1" name="patrimonio[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1" name="patrimonio[]"></textarea></div>
 						<div class="col-4 border border-success rounded-circle p-3">
 							<p class="text-center">Patromonio</p>
 						</div>
-						<div class="col-4 align-self-center"><textarea class="form-control area1" name="patrimonio[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area1" name="patrimonio[]"></textarea></div>
 					</div>
 					<div class="row justify-content-center">
-						<div class="col-4 align-self-center"><textarea class="form-control area" name="patrimonio[]"></textarea></div>
+						<div class="col-4 align-self-center"><textarea required class="form-control area" name="patrimonio[]"></textarea></div>
 					</div>
 				</div>
 				@endif
