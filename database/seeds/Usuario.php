860c154bd3eab6763f00116e618dfe9cafcec13d
josemg23 <josegri_magallanes@hotmail.com>
2020-11-18 10:47:11 -0500

    cambios

--- database/seeds/Usuario.php
+++ database/seeds/Usuario.php
@@ -97,14 +97,7 @@ class Usuario extends Seeder
             'updated_at'      => now()
             ]);
 
-            DB::table('role_user')->insert([
-           'role_id'    => 1,
-           'user_id'    => 1,
-           'created_at' => now(),
-           'updated_at' => now()
-        ]);
-
-            DB::table('role_user')->insert([
+           DB::table('role_user')->insert([
            'role_id'    => 2,
            'user_id'    => 2,
            'created_at' => now(),
