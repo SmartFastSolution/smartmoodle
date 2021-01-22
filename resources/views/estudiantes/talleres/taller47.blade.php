d678b53819af4dfaa56a404f5155c6f0fbdfaffb
DESKTOP-JGMS <josegri_magallanes@hotmail.com>
2021-01-22 01:00:54 -0500

    aa

--- resources/views/estudiantes/talleres/taller47.blade.php
+++ resources/views/estudiantes/talleres/taller47.blade.php
@@ -22,16 +22,11 @@
            </div>
            <div class="col-8">
                <ul class="list-group list-group-flush ">
-                @foreach ($taller->definiciones as $definicion)
+               @foreach ($taller->definiciones as $definicion)
                  
-                  <li class="list-group-item bg-transparent border-bottom-0 text-justify"><span class="badge badge-info">{{ $letra++ }}. </span> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
-                    
-                @endforeach
+                 <li class="list-group-item bg-transparent border-bottom-0 text-justify" style="font-size: 20px"><span class="badge badge-info">{{ $letra++ }}. </span>  {{ $definicion->definicion }}</li>    
+                  
+              @endforeach
                 </ul>
            </div>
        </div>
